10d40ec53f654dbf68a17b551152363b81362d6b
chanreaksanuth@gmail.com <chanreaksanuth@gmail.com>
2025-04-19 01:42:34 +0700

    new lesson

---
+++ src/main/java/co/istad/thymeleaf/webapp/ThymeleafWebappApplication.java
@@ -0,0 +1,13 @@
+package co.istad.thymeleaf.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafWebappApplication.class, args);
+    }
+
+}
